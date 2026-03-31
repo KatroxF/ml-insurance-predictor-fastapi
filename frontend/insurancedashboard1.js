fdd773edc3a27fcdba3cb52b83660054cce3c38a
KatroxF <faizan.abrar2004@gmail.com>
2026-03-31 23:01:11 +0530

    api_url1

--- frontend/insurancedashboard1.js
+++ frontend/insurancedashboard1.js
@@ -2,7 +2,7 @@
 
 const API_BASE=window.location.hostname=="localhost"
 ?"http://127.0.0.1:8000"
-:"https://ml-insurance-predictor-fastapi.onrender.com"
+:"https://insurance-backend-ewkb.onrender.com"
 
 
 function getToken() {
